75038fa34106c4f07e7fdc5768ca1a365fd094c7
Baggi <39710037+0xbaggi@users.noreply.github.com>
2025-02-11 15:50:59 +0100

    UI Updates

--- lib/tabs/googleVoice.dart
+++ lib/tabs/googleVoice.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:liquid_galaxy_ui_components/utils/constant.dart';
 
@@ -11,48 +12,29 @@ class GoogleVoiceUI extends StatefulWidget {
 class _GoogleVoiceUIState extends State<GoogleVoiceUI>
     with TickerProviderStateMixin {
   late AnimationController _appearController;
-  late AnimationController _disappearController;
-  final int _sections = 4;
-  late TweenSequence<Color?> _colorTweenSequence;
+  late AnimationController _rectanglesController;
 
   @override
   void initState() {
     super.initState();
 
+    // Controller for the text "reveal" animation.
     _appearController = AnimationController(
       vsync: this,
       duration: const Duration(seconds: 4),
     )..repeat();
 
-    _disappearController = AnimationController(
+    // Controller for the rectangles animation.
+    _rectanglesController = AnimationController(
       vsync: this,
-      duration: const Duration(seconds: 3),
-    )..repeat(reverse: true);
-
-    _colorTweenSequence = TweenSequence<Color?>([
-      TweenSequenceItem(
-        tween: ColorTween(begin: Colors.red, end: Colors.green),
-        weight: 1,
-      ),
-      TweenSequenceItem(
-        tween: ColorTween(begin: Colors.green, end: Colors.blue),
-        weight: 1,
-      ),
-      TweenSequenceItem(
-        tween: ColorTween(begin: Colors.blue, end: Colors.purple),
-        weight: 1,
-      ),
-      TweenSequenceItem(
-        tween: ColorTween(begin: Colors.purple, end: Colors.red),
-        weight: 1,
-      ),
-    ]);
+      duration: const Duration(seconds: 2),
+    )..repeat();
   }
 
   @override
   void dispose() {
     _appearController.dispose();
-    _disappearController.dispose();
+    _rectanglesController.dispose();
     super.dispose();
   }
 
@@ -67,9 +49,11 @@ class _GoogleVoiceUIState extends State<GoogleVoiceUI>
             decoration: BoxDecoration(
               color: primaryColor,
               borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+                topLeft: Radius.circular(20),
+                topRight: Radius.circular(20),
+              ),
             ),
-            height: 200,
+            height: 220,
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
@@ -102,32 +86,82 @@ class _GoogleVoiceUIState extends State<GoogleVoiceUI>
                   },
                 ),
                 const Spacer(),
-                AnimatedBuilder(
-                  animation: _disappearController,
-                  builder: (context, child) {
-                    return Container(
-                      height: 10,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(0),
-                        gradient: LinearGradient(
-                          begin: Alignment.centerLeft,
-                          end: Alignment.centerRight,
-                          colors: List.generate(_sections, (index) {
-                            return _colorTweenSequence.evaluate(
-                                AlwaysStoppedAnimation(
-                                    (_disappearController.value + index * 0.1) %
-                                        1))!;
-                          }),
-                        ),
-                      ),
-                    );
-                  },
-                ),
+                // The continuously looping animated rectangles.
+                _AnimatedRectangles(controller: _rectanglesController),
+                const SizedBox(height: 10),
               ],
             ),
           ),
         ],
       ),
     );
+  }
+}
+
+class _AnimatedRectangles extends StatelessWidget {
+  final AnimationController controller;
+
+  const _AnimatedRectangles({super.key, required this.controller});
+
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    return SizedBox(
+      width: screenWidth,
+      height: 30,
+      child: AnimatedBuilder(
+        animation: controller,
+        builder: (context, child) {
+          double t = controller.value;
+          const double amplitude = 0.3;
+
+          List<double> pulses = List.generate(4, (index) {
+            double phase = index / 4.0; // 0, 0.25, 0.5, 0.75
+            return 1.0 + amplitude * sin(2 * pi * (t + phase));
+          });
+
+          double sumPulses = pulses.fold(0.0, (prev, value) => prev + value);
+          List<double> widths =
+              pulses.map((pulse) => (pulse / sumPulses) * screenWidth).toList();
+
+          return Row(
+            children: List.generate(4, (index) {
+              return Container(
+                width: widths[index],
+                height: 10,
+                decoration: BoxDecoration(
+                  color: _getColor(index),
+                  boxShadow: [
+                    BoxShadow(
+                      color: _getColor(index).withAlpha((160).toInt()),
+                      blurRadius: 5,
+                      spreadRadius: 1,
+                      offset: const Offset(0, -3.5),
+                    )
+                  ],
+                ),
+              );
+            }),
+          );
+        },
+      ),
+    );
+  }
+
+  // Rectangle colors.
+  Color _getColor(int index) {
+    switch (index) {
+      case 0:
+        return Colors.blue;
+      case 1:
+        return Colors.red;
+      case 2:
+        return Colors.yellow;
+      case 3:
+        return Colors.green;
+      default:
+        return Colors.white;
+    }
   }
 }
--- lib/tabs/progresBar.dart
+++ lib/tabs/progresBar.dart
@@ -1,9 +1,8 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:liquid_galaxy_ui_components/utils/constant.dart';
 
 class ProgressBarPage extends StatefulWidget {
-  const ProgressBarPage({super.key});
+  const ProgressBarPage({Key? key}) : super(key: key);
 
   @override
   _ProgressBarPageState createState() => _ProgressBarPageState();
@@ -11,31 +10,30 @@ class ProgressBarPage extends StatefulWidget {
 
 class _ProgressBarPageState extends State<ProgressBarPage> {
   int _currentStep = 0;
-  late Timer _timer;
 
   final Color activeColor = Colors.lightGreen;
   final Color passedColor = Colors.green;
   final Color notPassedColor = Colors.grey;
-  final steps = ['Cart', 'Address', 'Payment', 'Checkout', 'Done'];
+  final List<String> steps = ['Cart', 'Address', 'Payment', 'Checkout', 'Done'];
 
-  @override
-  void initState() {
-    super.initState();
-    _startLoop();
-  }
+  static const double maxCircleSize = 35;
+  static const double lineWidth = 25;
+  static const double inactiveCircleSize = 30;
 
-  void _startLoop() {
-    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+  void _goToNextStep() {
+    if (_currentStep < steps.length - 1) {
       setState(() {
-        _currentStep = (_currentStep + 1) % 5; // Loop through 0 to 4
+        _currentStep++;
       });
-    });
+    }
   }
 
-  @override
-  void dispose() {
-    _timer.cancel();
-    super.dispose();
+  void _goToPreviousStep() {
+    if (_currentStep > 0) {
+      setState(() {
+        _currentStep--;
+      });
+    }
   }
 
   @override
@@ -47,66 +45,122 @@ class _ProgressBarPageState extends State<ProgressBarPage> {
         children: [
           const Spacer(),
           Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: List.generate(5, (index) {
-              bool isActive = index == _currentStep;
-              bool isPassed = index < _currentStep;
-
-              Color circleColor;
-              if (isActive) {
-                circleColor = activeColor;
-              } else if (isPassed) {
-                circleColor = passedColor;
-              } else {
-                circleColor = notPassedColor;
-              }
-
-              return Column(
-                children: [
-                  SizedBox(
-                      height: 60,
-                      child: AnimatedContainer(
-                        duration: const Duration(milliseconds: 300),
-                        width: isActive ? 35 : 30,
-                        height: isActive ? 35 : 30,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: circleColor,
-                        ),
-                        child: isActive
-                            ? Center(
-                                child: Icon(
-                                Icons.check,
-                                color: isActive
-                                    ? Colors.white
-                                    : Colors.transparent,
-                              ))
-                            : Center(
-                                child: Text((index + 1).toString(),
-                                    style:
-                                        const TextStyle(color: Colors.white)),
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              for (int i = 0; i < steps.length; i++) ...[
+                SizedBox(
+                  width: maxCircleSize,
+                  height: maxCircleSize,
+                  child: Center(
+                    child: AnimatedContainer(
+                      duration: const Duration(milliseconds: 300),
+                      width: (i == _currentStep)
+                          ? maxCircleSize
+                          : inactiveCircleSize,
+                      height: (i == _currentStep)
+                          ? maxCircleSize
+                          : inactiveCircleSize,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: _getCircleColor(i),
+                      ),
+                      child: (i == _currentStep)
+                          ? const Icon(Icons.check, color: Colors.white)
+                          : Center(
+                              child: Text(
+                                '${i + 1}',
+                                style: const TextStyle(color: Colors.white),
                               ),
-                      )),
-                  Text(
-                    steps[index],
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight:
-                          isActive ? FontWeight.bold : FontWeight.normal,
+                            ),
                     ),
                   ),
+                ),
+                if (i < steps.length - 1) ...[
+                  const SizedBox(width: 10),
+                  Container(
+                    width: lineWidth,
+                    height: 3,
+                    color: _getLineColor(i),
+                  ),
+                  const SizedBox(width: 10),
                 ],
-              );
-            }),
+              ],
+            ],
+          ),
+          const SizedBox(height: 8),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              for (int i = 0; i < steps.length; i++) ...[
+                SizedBox(
+                  width: maxCircleSize + 20 + lineWidth,
+                  child: Text(
+                    steps[i],
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: (i == _currentStep)
+                          ? FontWeight.bold
+                          : FontWeight.normal,
+                    ),
+                  ),
+                ),
+              ],
+            ],
           ),
           const Spacer(),
-          Text(
-            "Step ${_currentStep + 1} of 5",
-            style: const TextStyle(fontSize: 18, color: Colors.white),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: primaryColor,
+                    foregroundColor: Colors.white,
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 10),
+                    textStyle: const TextStyle(fontSize: 16),
+                  ),
+                  onPressed: _goToPreviousStep,
+                  child: const Text('Previous'),
+                ),
+                Text(
+                  "Step ${_currentStep + 1} of ${steps.length}",
+                  style: const TextStyle(fontSize: 18, color: Colors.white),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: primaryColor,
+                    foregroundColor: Colors.white,
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 10),
+                    textStyle: const TextStyle(fontSize: 16),
+                  ),
+                  onPressed: _goToNextStep,
+                  child: const Text('Next'),
+                ),
+              ],
+            ),
           ),
-          const SizedBox(height: 20),
+          const SizedBox(height: 40),
         ],
       ),
     );
+  }
+
+  Color _getCircleColor(int index) {
+    if (index == _currentStep) {
+      return activeColor;
+    } else if (index < _currentStep) {
+      return passedColor;
+    } else {
+      return notPassedColor;
+    }
+  }
+
+  Color _getLineColor(int index) {
+    return (_currentStep > index) ? passedColor : notPassedColor;
   }
 }
